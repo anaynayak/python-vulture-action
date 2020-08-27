4504568fdd02951be95ff67fc084692de1964dd5
Anay Nayak <anayak007@gmail.com>
2020-08-27 20:38:09 +0530

    Pass all args to vulture, fail on error

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,7 @@
-#!/bin/sh -l
+#!/bin/sh
 
-root_dir=$1
-vulture $root_dir | python /scripts/vulture.py
-echo "::error file=app.js,line=10,col=15::Something went wrong"
+args=$*
+resp=$(vulture $args)
+exitcode=$?
+echo "$resp" | python /scripts/vulture.py
+exit $exitcode
--- scripts/vulture.py
+++ scripts/vulture.py
@@ -1,5 +1,12 @@
 import sys
 
-for line in sys.stdin:
+def process(line):
   filename, line_num, text = line.strip().split(':')
   print(f"::error file={filename},line={line_num}::{text.strip()}")
+
+for line in sys.stdin:
+  try:
+    process(line)
+  except:
+    print(line)
+    sys.exit(1)
